9fa94d295999f89b3d6accfbcaa8d33fa409bad4
Ruby <106152821+RubyPrajapati@users.noreply.github.com>
2022-05-24 16:55:25 +0530

    Update enguiry.php

--- enguiry.php
+++ enguiry.php
@@ -4,70 +4,132 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>enquery form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <title>enquiry form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="fulldbms.css">
+<script src="fulldbms1.js"></script>
+
 </head>
-<style>
-    *{
-        background-image: linear-gradient(to left,rgb(173, 173, 70),rgb(119, 90, 37));
-    }
-    .para1{
-        line-height: 2.6; 
-    }
-    
-</style>
 <body>
-    <div class="container">
-        <div class="header">
-            <h1 class=" para font-weight-bold">Enqery Form</h1>
-            <p class="para1 font-weight-bold">please fill in your information and will the sending your order in no time</p>
-            <hr>
-            <form action="config.php" method="post">
-             <div class="form-group">
-                 <div class="col-lg-3">
-                     <label class="font-weight-bold">Your Name</label>
-                     <input type="text" name="name" placeholder="enter your name" class="form-control font-weight-bold" required="">   
-                 </div>
-             </div>
-             <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="font-weight-bold">Your email</label>
-                    <input type="email" name="emails" placeholder="enter your email" class="form-control font-weight-bold" required="">   
+    <div class="fluid-container">
+        <div class="col-lg-8">
+        <h1 class="text-primary font-weight-bold">Green Advanced Manufacturing Collaborative</h1>
+        <h3 class="text-dark font-weight-bold">Student Enquiery Form</h3>
+        <form action="" method="post" onsubmit="return validate()">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                    Username:<input type="text" name="name" placeholder="username" class="form-control" autocomplete="off" id="user"><span id="username"></span>
+                    </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                   Date: <input type="data" name="date" placeholder="enter a date" class="form-control " autocomplete="off" id="date"><span id="dates"></span>
+                   </div>
+                   </div>
+                   <div class="row">
+                   <div class="col-lg-6">
+                       <div class="form-group">
+                   Phone-No:<input type="number" name="num" placeholder="Phone num" class="form-control" autocomplete="off" id="phone"><span id="p-num"></span>
+                   </div>
+                   </div>
+                   <div class="col-lg-6">
+                       <div class="form-group">
+                   Ulternate-Num: <input type="number" name="ult" placeholder="ulternate num" class="form-control" autocomplete="off" id="ulternate"><span id="ulter"></span>
+                   </div>
+                   </div>
+                   <div class="row">
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                   Email-Address: <input type="email" name="email" placeholder="enter a email" class="form-control" autocomplete="off" id="email"><span id="emails"></span>
+                   </div>
+                   </div>
+                   <div class="col-lg-6">
+                       <div class="form-group">
+                   Home-Address: <input type="text" name="hom" placeholder="enter a address" class="form-control" autocomplete="off" id="homes"><span id="home"></span>
+                   </div>
+                   </div>
+                   <div class="row">
+                       <div class="col-lg-8">
+                           <div class="form-group">
+                   Employment Address:
+                       <input type="checkbox" name="chk[]" value="part time" id="em">Part Time
+                       <input type="checkbox" name="chk[]" value="full time" id="em">Full Time
+                       <input type="checkbox" name="chk[]" value="part time" id="em">Unemployed
+                       </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                Company Name: <input type="text" name="com" placeholder="Comapany name" class="form-control" autocomplete="off" id="com"><span id="comp"></span>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                Are you comapny vertern:?
+                                <input type="checkbox" name="chk1" value="yes" id="check">yes
+                                <input type="checkbox" name="chk1" value="No" id="check">No
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <div class="form-group">
+                                    Your English skills are:?
+                                    <input type="checkbox" name="chk2" value="Beginner" id="chk">Beginner
+                                    <input type="checkbox" name="chk2" value="Intermidate" id="chk">Intermidate
+                                    <input type="checkbox" name="chk2" value="Advanced" id="chk">Advanced
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <div class="form-group">
+                                        Are you Eligible Work in U.S:?
+                                        <input type="checkbox" name="chk3" value="Yes" id="ch">Yes
+                                        <input type="checkbox" name="chk3" value="No" id="ch">No
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            Father Name: <input type="text" name="fat" placeholder="Father name" autocomplete="off" class="form-control" id="fat"><span id="fath"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            Mother Name: <input type="text" name="mot" placeholder="Mother name" autocomplete="off" class="form-control" id="mo"><span id="ma"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-lg-8">
+                                            <div class="form-group">
+                                                Highest Degree you Have Earned:?
+                                                <input type="checkbox" name="chk4" value="UG" id="ug">UG
+                                                <input type="checkbox" name="chk4" value="PG" id="ug">PG
+                                                <input type="checkbox" name="chk4" value="Diploma" id="ug">Diploma
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    How did you learn about program:?
+                                                    <input type="text" name="pro" placeholder="program about" class="form-control" autocomplete="off" id="pro"><span id="program"></span><br>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="font-weight-bold">Your password</label>
-                    <input type="password" name="password"  placeholder="enter your password" class="form-control font-weight-bold" required="">   
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="font-weight-bold">Your contact</label>
-                    <input type="number" name="num" placeholder="enter your contact" class="form-control font-weight-bold" required="">   
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="font-weight-bold">Your dob</label>
-                    <input type="date" name="dob" placeholder="enter your dob" class="form-control font-weight-bold" required="">   
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-3  font-weight-bold">
-                Gender:
-      <input type="radio" name="gender"  value="female">Female
-      <input type="radio" name="gender"   value="male">Male
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="font-weight-bold">comment</label>
-                    <textarea name="textarea" name="comment" placeholder="enter your fill details" class="form-control font-weight-bold" required="">   
-                </textarea>
-            </div>
-            <input type="submit" class="btn btn-warning mt-3 font-weight-bold" value="submit" name="submit">
-            </form>
+            </div><br>
+            <input type="submit" name="submit" value="submit" class="bg-success col-sm-1 me-5 mb-2">
+            
+        </form>
         </div>
     </div>
 </body>
